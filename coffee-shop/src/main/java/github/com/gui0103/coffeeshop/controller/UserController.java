b42b30ba5fa0445a9c2c132f065c6f92df38b2fb
gui0103 <gui03costa>
2022-09-18 03:52:42 -0300

    adding http status for UserController

--- coffee-shop/src/main/java/github/com/gui0103/coffeeshop/controller/UserController.java
+++ coffee-shop/src/main/java/github/com/gui0103/coffeeshop/controller/UserController.java
@@ -3,11 +3,14 @@ package github.com.gui0103.coffeeshop.controller;
 import github.com.gui0103.coffeeshop.entity.User;
 import github.com.gui0103.coffeeshop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.http.ResponseEntity.status;
 
 @RestController
 @RequestMapping("/users")
@@ -17,17 +20,26 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping
-    public List<User> findAllUsers() {
-        return userRepository.findAll();
+    public ResponseEntity<List<User>> findAllUsers() {
+
+        return status(200).body(userRepository.findAll());
     }
 
     @GetMapping("/{id}")
-    public Optional<User> findUserById(@PathVariable String id) {
-        return userRepository.findById(id);
+    public ResponseEntity<Optional<User>> findUserById(@PathVariable String id) {
+
+        if (!userRepository.existsById(id)) {
+            return ResponseEntity.status(404).build();
+        }
+
+        return status(200).body(userRepository.findById(id));
     }
 
     @PostMapping
-    public void createUser(@Valid @RequestBody User user) {
+    public ResponseEntity createUser(@Valid @RequestBody User user) {
+
         userRepository.save(user);
+
+        return status(201).build();
     }
 }
